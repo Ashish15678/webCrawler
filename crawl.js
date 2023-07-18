660bdc6694501980d1c1aa28a7ed2ea475a7774d
Ashish Kumar <15678ashish@gmail.com>
2023-07-18 21:07:08 +0530

    crawl single page with error intitalized

--- crawl.js
+++ crawl.js
@@ -1,5 +1,31 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentUrl) {
+  console.log(`Currently crawling ${currentUrl}`);
+
+  try {
+    const resp = await fetch(currentUrl);
+
+    if (resp.status > 399) {
+      console.log(
+        `error in fetch with status code ${resp.status} at page: ${currentUrl}`
+      );
+      return;
+    }
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `non HTML response , content-type: ${contentType} on page: ${currentUrl}`
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+  } catch (error) {
+    console.log(`Error found while crawling: ${currentUrl}`);
+  }
+}
+
 function getUrlFromHTML(htmlBody, baseUrl) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -28,4 +54,5 @@ function normalizeUrl(urlString) {
 module.exports = {
   normalizeUrl,
   getUrlFromHTML,
+  crawlPage,
 };
--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-console.log("hello world");
-console.log("This works");
+const { crawlPage } = require("./crawl");
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("too many inputs");
+    process.exit(1);
+  }
+  const baseUrl = process.argv[2];
+  console.log(baseUrl);
+  crawlPage(baseUrl);
+}
+
+main();
